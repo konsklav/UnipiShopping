14a378caacfa27627886345c34d07f4293471578
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-03 19:31:59 +0200

    Ported AppActivityBase and SettingsService from AudioStories project

---
+++ app/src/main/java/com/example/unipishopping/core/SettingsService.java
@@ -0,0 +1,40 @@
+package com.example.unipishopping.core;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.annotation.Nullable;
+import androidx.core.os.LocaleListCompat;
+
+public class SettingsService {
+
+    private static final String PREF_NAME = "UniPi-Shopping_SETTINGS";
+    private static final String LANG_KEY = "lang";
+
+    /**
+     * Get the currently used Locale by the application.
+     * @return The current locale stored in local storage..
+     */
+    public static LocaleListCompat getLocale(Context context) {
+        SharedPreferences sp = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        String languageCode = sp.getString(LANG_KEY, null);
+
+        if (languageCode == null)
+            return LocaleListCompat.getEmptyLocaleList();
+
+        return LocaleListCompat.forLanguageTags(languageCode);
+    }
+
+    /**
+     * Saves the desired Locale in local storage.
+     * @param languageCode The two letter language code of the locale you want to use.
+     *                     Specify NULL if you want to use the default locale.
+     */
+    public static void setLocale(Context context, @Nullable String languageCode) {
+        SharedPreferences sp = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.putString(LANG_KEY, languageCode);
+        editor.apply();
+    }
+}
---
+++ app/src/main/java/com/example/unipishopping/ui/AppActivityBase.java
@@ -0,0 +1,65 @@
+package com.example.unipishopping.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.graphics.Insets;
+import androidx.core.os.LocaleListCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewbinding.ViewBinding;
+
+import com.example.unipishopping.R;
+import com.example.unipishopping.core.SettingsService;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+
+public abstract class AppActivityBase<TBinding extends ViewBinding> extends AppCompatActivity {
+
+    private TBinding binding;
+    protected TBinding getBinding() { return binding; }
+
+    @Override
+    protected final void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        // Inflate the ViewBinding so that all Views (Buttons, TextViews, etc...) are initialized.
+        binding = inflateBinding();
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Get the Locale from storage and refresh the Activity if it's not the default
+        LocaleListCompat appLocale = SettingsService.getLocale(this);
+        AppCompatDelegate.setApplicationLocales(appLocale);
+
+        onAfterCreate();
+    }
+
+    /**
+     * Use reflection to dynamically invoke ViewBinding.inflate(getLayoutInflater()) and
+     * avoid having to declare an abstract method for this.
+     */
+    private TBinding inflateBinding() {
+        try {
+            ParameterizedType superClass = (ParameterizedType) getClass().getGenericSuperclass();
+            Class<TBinding> bindingClass = (Class<TBinding>) superClass.getActualTypeArguments()[0];
+
+            Method inflateMethod = bindingClass.getMethod("inflate", LayoutInflater.class);
+            return (TBinding) inflateMethod.invoke(null, getLayoutInflater());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to inflate ViewBinding in " + getClass().getSimpleName(), e);
+        }
+    }
+    protected abstract void onAfterCreate();
+}
