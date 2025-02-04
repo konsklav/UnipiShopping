32009a3fa119517622eb6a7670fdc034939774ed
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-04 23:02:32 +0200

    Add some temp code in MainActivity to test out Parcelable

--- app/src/main/java/com/example/unipishopping/ui/MainActivity.java
+++ app/src/main/java/com/example/unipishopping/ui/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.unipishopping.ui;
 
+import android.util.Log;
+
 import com.example.unipishopping.databinding.ActivityMainBinding;
+import com.example.unipishopping.domain.User;
+import com.example.unipishopping.ui.constants.IntentExtras;
 
 public class MainActivity extends AppActivityBase<ActivityMainBinding> {
     @Override
     protected void onAfterCreate() {
 
+        User user = getIntent().getParcelableExtra(IntentExtras.USER_PARCELABLE);
+
+        Log.i("MAIN ACTIVITY", "Got user '" + user.getUsername() + "'!");
     }
 }
