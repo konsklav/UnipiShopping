444bde5dac51749b2d2a73d153ab0ca7b7b43a63
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-02-04 17:52:10 +0200

    Updated domain model to follow Firebase needs

--- app/src/main/java/com/example/unipishopping/domain/Purchase.java
+++ app/src/main/java/com/example/unipishopping/domain/Purchase.java
@@ -1,20 +1,22 @@
 package com.example.unipishopping.domain;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.TimeZone;
 
 public class Purchase {
     int productId;
-    LocalDateTime timestamp;
+    long timestamp;
 
     // Required for Firebase
     private Purchase() {}
 
-    public Purchase(int productId, LocalDateTime timestamp) {
+    public Purchase(int productId, long timestamp) {
         this.productId = productId;
         this.timestamp = timestamp;
     }
 
     // Required for Firebase
     public int getProductId() { return productId; }
-    public LocalDateTime getTimestamp() { return this.timestamp; }
+    public long getTimestamp() { return this.timestamp; }
 }
