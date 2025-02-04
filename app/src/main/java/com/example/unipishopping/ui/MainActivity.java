010576a6655afeeea0aa28872e6001992934acd3
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-04 21:57:57 +0200

    Refactor ALL activities to use custom activity base class

--- app/src/main/java/com/example/unipishopping/ui/MainActivity.java
+++ app/src/main/java/com/example/unipishopping/ui/MainActivity.java
@@ -1,26 +1,10 @@
 package com.example.unipishopping.ui;
 
-import android.os.Bundle;
+import com.example.unipishopping.databinding.ActivityMainBinding;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.example.unipishopping.R;
-
-public class MainActivity extends AppCompatActivity{
-
+public class MainActivity extends AppActivityBase<ActivityMainBinding> {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    protected void onAfterCreate() {
+
     }
 }
--- app/src/main/java/com/example/unipishopping/ui/ProductActivity.java
+++ app/src/main/java/com/example/unipishopping/ui/ProductActivity.java
@@ -1,26 +1,10 @@
 package com.example.unipishopping.ui;
 
-import android.os.Bundle;
+import com.example.unipishopping.databinding.ActivityProductBinding;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.example.unipishopping.R;
-
-public class ProductActivity extends AppCompatActivity {
-
+public class ProductActivity extends AppActivityBase<ActivityProductBinding> {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_product);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    protected void onAfterCreate() {
+
     }
 }
