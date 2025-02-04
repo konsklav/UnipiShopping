c22876c74186aff0d784ff1d67fbaee0eefa98b2
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-04 22:58:43 +0200

    Make onLoginFail() return LoginError that provides helpful details

--- app/src/main/java/com/example/unipishopping/core/LoginCallbacks.java
+++ app/src/main/java/com/example/unipishopping/core/LoginCallbacks.java
@@ -3,6 +3,6 @@ package com.example.unipishopping.core;
 import com.example.unipishopping.domain.User;
 
 public interface LoginCallbacks {
-    void onSuccess(User user);
-    void onFail();
+    void onLoginSuccess(User user);
+    void onLoginFail(LoginError error);
 }
