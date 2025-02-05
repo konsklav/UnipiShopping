2d0a4e2a3ab53fde651bef7a3ab2c440b46ae14b
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-05 10:41:23 +0200

    Partially implement ProductActivity

--- app/src/main/java/com/example/unipishopping/ui/ProductActivity.java
+++ app/src/main/java/com/example/unipishopping/ui/ProductActivity.java
@@ -1,10 +1,28 @@
 package com.example.unipishopping.ui;
 
+import android.util.Log;
+import android.widget.Toast;
+
 import com.example.unipishopping.databinding.ActivityProductBinding;
+import com.example.unipishopping.domain.Product;
+import com.example.unipishopping.ui.constants.IntentExtras;
+import com.example.unipishopping.ui.formats.NumFormatter;
 
 public class ProductActivity extends AppActivityBase<ActivityProductBinding> {
     @Override
     protected void onAfterCreate() {
+        Product product = getIntent().getParcelableExtra(IntentExtras.PRODUCT_PARCELABLE);
+        if (product == null) {
+            Log.e("Product Activity", "Product parcelable is NULL!");
+            return;
+        }
 
+        getBinding().ivProductImg.setImageResource(product.getImageId());
+        getBinding().tvProductName.setText(product.getTitleId());
+        getBinding().tvProductPrice.setText(NumFormatter.formatAsPrice(product.getPrice(), getLocale()));
+
+        getBinding().ivBuyButton.setOnClickListener(v -> {
+            Toast.makeText(this, "TEMP: You bought this product!!!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
