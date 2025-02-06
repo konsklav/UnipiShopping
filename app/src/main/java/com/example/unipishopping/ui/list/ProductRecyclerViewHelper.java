0bd0b8dd9efd27b999c4803f9d233bed0862fc8e
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-07 00:13:53 +0200

    Add conditional orientation linear layout manager

--- app/src/main/java/com/example/unipishopping/ui/list/ProductRecyclerViewHelper.java
+++ app/src/main/java/com/example/unipishopping/ui/list/ProductRecyclerViewHelper.java
@@ -1,15 +1,25 @@
 package com.example.unipishopping.ui.list;
 
 import android.content.Context;
+import android.content.res.Configuration;
 
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ProductRecyclerViewHelper {
     public static void applyStyling(RecyclerView recyclerView, Context context) {
         recyclerView.addItemDecoration(new ProductItemDecoration(10));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setLayoutManager(new GridLayoutManager(context, 1));
+
+        Configuration config = context.getResources().getConfiguration();
+
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(
+                        context, config.orientation == Configuration.ORIENTATION_LANDSCAPE
+                        ? LinearLayoutManager.HORIZONTAL
+                        : LinearLayoutManager.VERTICAL, false));
+
     }
 }
