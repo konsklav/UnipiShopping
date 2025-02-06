102ad7646cc8b0e71fab629d21fd086710492994
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-07 00:23:09 +0200

    Cleanup

--- app/src/main/java/com/example/unipishopping/ui/list/ProductRecyclerViewHelper.java
+++ app/src/main/java/com/example/unipishopping/ui/list/ProductRecyclerViewHelper.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.Configuration;
 
 import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
