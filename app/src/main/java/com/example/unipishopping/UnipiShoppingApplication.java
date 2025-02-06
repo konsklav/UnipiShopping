2ff1a4701ed7ac759a1410b4a71b72181c3afa8a
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-06 19:56:00 +0200

    Initialize NotificationService on application startup

--- app/src/main/java/com/example/unipishopping/UnipiShoppingApplication.java
+++ app/src/main/java/com/example/unipishopping/UnipiShoppingApplication.java
@@ -2,9 +2,14 @@ package com.example.unipishopping;
 
 import android.app.Application;
 
+import com.example.unipishopping.core.NotificationService;
+
 public class UnipiShoppingApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        NotificationService notificationService = new NotificationService();
+        notificationService.start(this);
     }
 }
