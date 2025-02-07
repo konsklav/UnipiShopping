741307987b742eaecee61ae6d78eef8548beb128
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-07 02:15:00 +0200

    Make (half-complete) SettingsActivity

--- app/src/main/java/com/example/unipishopping/ui/SettingsActivity.java
+++ app/src/main/java/com/example/unipishopping/ui/SettingsActivity.java
@@ -1,10 +1,36 @@
 package com.example.unipishopping.ui;
 
+import com.example.unipishopping.core.session.UserSession;
+import com.example.unipishopping.core.settings.SettingsService;
 import com.example.unipishopping.databinding.ActivitySettingsBinding;
+import com.example.unipishopping.domain.User;
+import com.example.unipishopping.domain.UserSettings;
 
 public class SettingsActivity extends AppActivityBase<ActivitySettingsBinding> {
     @Override
     protected void onAfterCreate() {
+        getBinding().btnEnglish.setOnClickListener(v -> changeLocale("en"));
+        getBinding().btnGreek.setOnClickListener(v -> changeLocale("el"));
+        getBinding().btnSpanish.setOnClickListener(v -> changeLocale("es"));
 
+        User user = UserSession.getInstance().getUser();
+        getBinding().tvUsername.setText(user.getUsername());
+        getBinding().tvPassword.setText(user.getPassword());
+    }
+
+    private void changeFontSize(int size) {
+        recreate();
+    }
+
+    private void changeBackground() {
+        recreate();
+    }
+
+    private void changeLocale(String languageCode) {
+        UserSettings settings = SettingsService.get(this);
+        settings.setLocaleLanguageCode(languageCode);
+        SettingsService.save(settings, this);
+
+        recreate();
     }
 }
